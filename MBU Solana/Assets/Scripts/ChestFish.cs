f792b64ec5133c9a7d3151bef522aba05099756d
Oliver766 <orl102@student.aru.ac.uk>
2024-04-16 20:24:44 +0100

    fixed a few things

--- MBU Solana/Assets/Scripts/ChestFish.cs
+++ MBU Solana/Assets/Scripts/ChestFish.cs
@@ -15,17 +15,18 @@ public class ChestFish : MonoBehaviour
     public Sprite chesto;
     public Sprite chestc;
     public SpriteRenderer SpriteRenderer;
+    public int versions;
 
 
     private void Awake()
     {
         chestOpened = (PlayerPrefs.GetInt("ChestopenFish") != 0);
+        versions = PlayerPrefs.GetInt("versions");
     }
 
     public void Start()
     {
-        chestOpened = (PlayerPrefs.GetInt("ChestopenFish") != 0);
-        if (chestOpened == true)
+        if (chestOpened == true || versions == 1)
         {
             SpriteRenderer.sprite = chesto;
             GetComponent<SpriteRenderer>().sprite = chesto;
@@ -33,7 +34,7 @@ public class ChestFish : MonoBehaviour
 
 
         }
-        else if (chestOpened == false)
+        else if (chestOpened == false || versions == 0)
         {
             SpriteRenderer.sprite = chestc;
             GetComponent<SpriteRenderer>().sprite = chestc;
@@ -45,7 +46,7 @@ public class ChestFish : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            if(chestOpened == false)
+            if(chestOpened == false )
             {
                 button.SetActive(true);
             }
@@ -61,7 +62,7 @@ public class ChestFish : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (chestOpened == false)
+            if (chestOpened == false )
             {
                 button.SetActive(false);
             }
@@ -80,6 +81,8 @@ public class ChestFish : MonoBehaviour
         button.SetActive(false);
         PlayerPrefs.SetInt("ChestopenFish", (chestOpened ? 1 : 0));
         chestOpened = true;
+        versions++;
+        PlayerPrefs.GetInt("versions",versions);
 
 
     }
@@ -96,8 +99,9 @@ public class ChestFish : MonoBehaviour
 
     public void Update()
     {
-        chestOpened = (PlayerPrefs.GetInt("ChestopenFish") != 0);
-        if (chestOpened == true)
+        PlayerPrefs.SetInt("ChestopenFish", (chestOpened ? 1 : 0));
+        PlayerPrefs.SetInt("versions", versions);
+        if (chestOpened == true || versions == 1)
         {
             SpriteRenderer.sprite = chesto;
             GetComponent<SpriteRenderer>().sprite = chesto;
@@ -105,7 +109,7 @@ public class ChestFish : MonoBehaviour
 
 
         }
-        else if(chestOpened == false)
+        else if (chestOpened == false || versions == 0)
         {
             SpriteRenderer.sprite = chestc;
             GetComponent<SpriteRenderer>().sprite = chestc;
