4721075c0c43245309a28de537481fbb21f99077
vaiv26 <vaibhavkumar651@gmail.com>
2024-05-22 18:28:44 +0530

    Spawn Points

--- MBU Solana/Assets/Scripts/Multiplayer/photonPlayer.cs
+++ MBU Solana/Assets/Scripts/Multiplayer/photonPlayer.cs
@@ -23,23 +23,36 @@ public class photonPlayer : MonoBehaviour
         // gets photon view component at start
         PV = GetComponent<PhotonView>();
 
-        // randomly chooses spawn point
-        int spawnPicker = 0;
-        if (GameSetUP.GS.spawnPoints[spawnPicker].GetBoolSpawnPoint())
+        if (PhotonNetwork.IsMasterClient)
         {
-            spawnPicker = 1;
-        }
-        //Setting the element's object's value of bool to be true in the struct
-        GameSetUP.GS.spawnPoints[spawnPicker].SetBoolSpawnPoint(true);
+            // randomly chooses spawn point
+            int spawnPicker = 0;
+            if (GameSetUP.GS.spawnPoints[spawnPicker].GetBoolSpawnPoint())
+            {
+                spawnPicker = 1;
+            }
+            //Setting the element's object's value of bool to be true in the struct
+            GameSetUP.GS.spawnPoints[spawnPicker].SetBoolSpawnPoint(true);
 
+            // Synchronize the spawn point selection with other clients
+            PV.RPC("RPC_SetSpawnPoint", RpcTarget.AllBuffered, spawnPicker);
+        }
+
+    }
+    #endregion
+
+    [PunRPC]
+    void RPC_SetSpawnPoint(int spawnPicker)
+    {
         if (PV.IsMine)
         {
-            // instantiates photon prefab over the network
-            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), GameSetUP.GS.spawnPoints[spawnPicker].GetTransformSpawnPoint().position, GameSetUP.GS.spawnPoints[spawnPicker].GetTransformSpawnPoint().rotation, 0);
+            // Instantiate the player avatar at the selected spawn point
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"),
+                GameSetUP.GS.spawnPoints[spawnPicker].GetTransformSpawnPoint().position,
+                GameSetUP.GS.spawnPoints[spawnPicker].GetTransformSpawnPoint().rotation, 0);
         }
         Debug.Log(GameSetUP.GS.spawnPoints[spawnPicker].GetBoolSpawnPoint());
     }
-    #endregion
 }
 
 
