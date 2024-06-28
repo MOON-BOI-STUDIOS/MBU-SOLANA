37fc01954116f2cb7eb9fd3b4a37cbb40975da23
Oliver766 <67057688+Oliver766@users.noreply.github.com>
2024-06-28 09:10:55 +0100

    updated attack cards

--- MBU Solana/Assets/Scripts/CardMechanic/CardDeckSsystem/BaseAction/CardObject.cs
+++ MBU Solana/Assets/Scripts/CardMechanic/CardDeckSsystem/BaseAction/CardObject.cs
@@ -7,9 +7,6 @@ public class CardObject : ScriptableObject
 {
     public string cardName;
     public Image art;
-    [TextArea(0,100 )]
-    public string description;
-    public int cost;
     public int damage;
     public int ID_BasicAction;
     public CardType cardType;
